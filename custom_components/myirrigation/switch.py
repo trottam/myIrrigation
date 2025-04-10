bef76ac32edb119e0e4dc788ff20667cf7ac4760
trottam <38915551+trottam@users.noreply.github.com>
2025-04-10 23:29:58 +0200

    Update switch.py

--- custom_components/myirrigation/switch.py
+++ custom_components/myirrigation/switch.py
@@ -1,10 +1,6 @@
 import logging
 import requests
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,36 +20,19 @@ HEADERS_LOGIN = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
 }
 
-def HEADERS_COMMAND(id_module, cookie):
-    return {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "it-IT,it;q=0.9,en;q=0.8",
-        "Connection": "keep-alive",
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Host": "www.mysolem.com",
-        "Origin": "https://www.mysolem.com",
-        "Referer": f"https://www.mysolem.com/module/{id_module}",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
-        "Cookie": cookie
-    }
-
-async def async_setup_entry(
-    hass: HomeAssistant,
-    config_entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback
-) -> None:
-    data = config_entry.data
-    switch = MyIrrigationSwitch(
-        username=data["username"],
-        password=data["password"],
-        zone=data["zone"],
-        module_id=data["id_module"],
-        serial_number=data["serial_number"]
-    )
-    async_add_entities([switch])
-
+HEADERS_COMMAND = lambda id_module, cookie: {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "it-IT,it;q=0.9,en;q=0.8",
+    "Connection": "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Host": "www.mysolem.com",
+    "Origin": "https://www.mysolem.com",
+    "Referer": f"https://www.mysolem.com/module/{id_module}",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest",
+    "Cookie": cookie
+}
 
 class MyIrrigationSwitch(SwitchEntity):
     def __init__(self, username, password, zone, module_id, serial_number):
@@ -65,41 +44,48 @@ class MyIrrigationSwitch(SwitchEntity):
         self.module_id = module_id
         self.serial_number = serial_number
 
+    async def async_turn_on(self, **kwargs):
+        await self.hass.async_add_executor_job(self._send_command, "on")
+        self._is_on = True
+        self.async_write_ha_state()
+
+    async def async_turn_off(self, **kwargs):
+        await self.hass.async_add_executor_job(self._send_command, "off")
+        self._is_on = False
+        self.async_write_ha_state()
+
     @property
-    def is_on(self) -> bool:
+    def is_on(self):
         return self._is_on
 
-    def turn_on(self, **kwargs):
-        self._send_command("on")
-        self._is_on = True
-        self.schedule_update_ha_state()
+    def _send_command(self, command):
+        try:
+            session = requests.Session()
+            session.get(COOKIE_URL)
+            cookie = session.cookies.get_dict()
+            cookie_str = "; ".join(f"{k}={v}" for k, v in cookie.items())
 
-    def turn_off(self, **kwargs):
-        self._send_command("off")
-        self._is_on = False
-        self.schedule_update_ha_state()
+            login_payload = {
+                "email": self.username,
+                "password": self.password,
+                "country-select": self.zone
+            }
 
-    def _send_command(self, command: str):
-        session = requests.Session()
-        session.get(COOKIE_URL)
-        cookie = session.cookies.get_dict()
-        cookie_str = "; ".join(f"{k}={v}" for k, v in cookie.items())
+            login_response = session.post(LOGIN_URL, headers={**HEADERS_LOGIN, "Cookie": cookie_str}, data=login_payload)
+            login_response.raise_for_status()
 
-        login_payload = {
-            "email": self.username,
-            "password": self.password,
-            "country-select": self.zone
-        }
+            command_payload = {
+                "moduleSerialNumber": self.serial_number,
+                "command": command,
+                "commandType": "status",
+                "commandParams": "0"
+            }
 
-        session.post(LOGIN_URL, headers={**HEADERS_LOGIN, "Cookie": cookie_str}, data=login_payload)
+            command_headers = HEADERS_COMMAND(self.module_id, cookie_str)
+            response = session.post(COMMAND_URL, headers=command_headers, data=command_payload)
+            response.raise_for_status()
 
-        command_payload = {
-            "moduleSerialNumber": self.serial_number,
-            "command": command,
-            "commandType": "status",
-            "commandParams": "0"
-        }
+            _LOGGER.debug("Comando inviato con successo: %s", response.text)
 
-        command_headers = HEADERS_COMMAND(self.module_id, cookie_str)
-        response = session.post(COMMAND_URL, headers=command_headers, data=command_payload)
-        _LOGGER.debug("Comando inviato: %s", response.text)
+        except requests.exceptions.RequestException as e:
+            _LOGGER.error("Errore durante l'invio del comando %s: %s", command, e)
