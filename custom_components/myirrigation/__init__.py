48b1fdd926923bf68bbacf4aaa588677e444cc36
trottam <38915551+trottam@users.noreply.github.com>
2025-04-11 12:37:29 +0200

    Update __init__.py

--- custom_components/myirrigation/__init__.py
+++ custom_components/myirrigation/__init__.py
@@ -1,15 +1,18 @@
 from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers import config_entry_flow
 
 DOMAIN = "myirrigation"
 
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    # Passaggio del flow dell'entry alla piattaforma 'switch'
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "switch")
     )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     return await hass.config_entries.async_forward_entry_unload(entry, "switch")
