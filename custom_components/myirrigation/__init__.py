ed6ff7c2fa53624f4bb835d3c06ce6f066de6202
trottam <38915551+trottam@users.noreply.github.com>
2025-04-18 12:27:56 +0200

    Update __init__.py

--- custom_components/myirrigation/__init__.py
+++ custom_components/myirrigation/__init__.py
@@ -1,18 +1,22 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers import config_entry_flow
+from .valve import async_setup_platform  # Importa la funzione di configurazione della piattaforma
 
 DOMAIN = "myirrigation"
 
 async def async_setup(hass: HomeAssistant, config: dict):
+    """Setup per la configurazione iniziale (quando non si usa un config entry)"""
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Passaggio del flow dell'entry alla piattaforma 'switch'
+    """Setup quando un config entry viene aggiunto"""
+    # Passaggio del flow dell'entry alla piattaforma 'valve'
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "switch")
+        hass.config_entries.async_forward_entry_setup(entry, "valve")
     )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "switch")
+    """Unloading quando un config entry viene rimosso"""
+    return await hass.config_entries.async_forward_entry_unload(entry, "valve")
